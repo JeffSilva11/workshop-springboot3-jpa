d372a6f23935979e09ac92cb3e006f0f8bfa84e7
Jefferson Silva <fj_adm@hotmail.com>
2023-05-14 15:53:47 -0300

    Project created

---
+++ src/main/java/curso/java/curso_298_projeto/Curso298ProjetoApplication.java
@@ -0,0 +1,13 @@
+package curso.java.curso_298_projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curso298ProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curso298ProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/curso/java/curso_298_projeto/Curso298ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.java.curso_298_projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curso298ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
